e7aea90b2f7845a18210005528378d897f7768cf
AVenturi98 <lssndrventuri@gmail.com>
2025-03-31 15:37:40 +0200

    ADD milestone 5

--- client/src/pages/AddTask.jsx
+++ client/src/pages/AddTask.jsx
@@ -1,7 +1,71 @@
+import { useState, useRef, useMemo } from "react";
+
 export default function AddTask() {
+
+    const [title, setTitle] = useState('');
+    const description = useRef();
+    const status = useRef();
+
+    const symbols = '!@#$%^&*()-_=+[]{}|;:\'\\,.<>?/`~';
+
+    const taskErrore = useMemo(() => {
+        if (!title.trim()) {
+            return 'Title is required'
+        }
+        if ([...title].some(e => symbols.includes(e))) {
+            return 'Title cannot contain special characters'
+        }
+        return ''
+    }, [title])
+
+    function submitForm(e) {
+        e.preventDefault();
+
+        if (taskErrore)
+            return;
+
+        const newTask = {
+            title: title.trim(),
+            description: description.current.value.trim(),
+            status: status.current.value,
+        }
+
+        console.log(newTask)
+
+    }
+
     return (
-        <>
-            Form nuovo Task
-        </>
+        <form onSubmit={submitForm}>
+            <h2>Title</h2>
+            <input
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            {taskErrore &&
+                <div className="error">{taskErrore}</div>
+            }
+            <h2>Description</h2>
+            <input
+                type="textarea"
+                placeholder="Descrizione"
+                ref={description}
+            />
+            <h2>Status</h2>
+            <select
+                name="status"
+                ref={status}>
+                {['To do', 'Doing', 'Done'].map((e, i) => (
+                    <option key={i} value={e}>{e}</option>
+                ))}
+            </select>
+            <button
+                type='button'
+                onClick={submitForm}
+                disabled={taskErrore}>
+                Aggiungi
+            </button>
+        </form>
     )
 }
