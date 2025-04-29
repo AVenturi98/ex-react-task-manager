9c31a32bf90c53304c09f4bf1d91fdb4f17890e5
AVenturi98 <lssndrventuri@gmail.com>
2025-04-29 15:45:58 +0200

    ADD milestone 12

--- client/src/pages/TaskList.jsx
+++ client/src/pages/TaskList.jsx
@@ -1,11 +1,31 @@
-import { useContext, useState, useMemo } from "react";
+import { useContext, useState, useMemo, useRef, useCallback } from "react";
 import GlobalContext from "../context/GlobalContext";
-
+// Components
 import TaskRow from "../components/TaskRow";
+
+const debounce = (callback, delay) => {
+    let timer;
+    return (value) => {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            callback(value);
+        }, delay);
+    };
+}
 
 export default function TaskList() {
 
     const { tasks } = useContext(GlobalContext);
+
+    const searchRef = useRef();
+
+    // Search value
+    const [searchQuery, setSearchQuery] = useState('');
+
+    // Debounce the search input
+    const debouncedSearch = useCallback(debounce(setSearchQuery, 500), []);
 
     // State for order
     const [sortBy, setSortBy] = useState('createdAt');
@@ -23,30 +43,43 @@ export default function TaskList() {
     const sortIcon = sortOrder === 1 ? '↑' : '↓';
 
     const sortedTasks = useMemo(() => {
-        return [...tasks].sort((a, b) => {
+        return [...tasks]
+            .filter(t => t.title.toLowerCase().includes(searchQuery.toLowerCase()))
+            .sort((a, b) => {
 
-            let comparison;
+                let comparison;
 
-            if (sortBy === 'title') {
-                comparison = a.title.localeCompare(b.title);
+                if (sortBy === 'title') {
+                    comparison = a.title.localeCompare(b.title);
 
-            } else if (sortBy === 'status') {
-                const statusOrder = ['To do', 'Doing', 'Done'];
-                comparison = statusOrder.indexOf(a.status) - statusOrder.indexOf(b.status);
+                } else if (sortBy === 'status') {
+                    const statusOrder = ['To do', 'Doing', 'Done'];
+                    comparison = statusOrder.indexOf(a.status) - statusOrder.indexOf(b.status);
 
-            } else if (sortBy === 'createdAt') {
-                comparison = new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
-            }
+                } else if (sortBy === 'createdAt') {
+                    comparison = new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+                }
 
-            return comparison * sortOrder;
-        })
+                return comparison * sortOrder;
+            })
 
-    }, [sortBy, sortOrder, tasks])
+    }, [sortBy, sortOrder, tasks, searchQuery])
+
 
 
 
     return (
         <>
+            <div className="searchInput">
+                <h2>Tasks</h2>
+                <input
+                    type="text"
+                    placeholder="Cerca task..."
+                    onChange={e => debouncedSearch(e.target.value)}
+                    ref={searchRef}
+                    style={{ margin: "20px 0" }}
+                />
+            </div>
 
             <table>
                 <thead>
